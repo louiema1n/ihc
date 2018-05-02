19a93898feb4861e217f708367473e3f0e478712
louiemain <louielcxy@live.cn>
2018-05-02 22:45:12 +0800

    项目第一次发布

--- src/main/java/com/lm/ihc/controller/IhcsController.java
+++ src/main/java/com/lm/ihc/controller/IhcsController.java
@@ -1,11 +1,18 @@
 package com.lm.ihc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lm.ihc.domain.Ihcs;
 import com.lm.ihc.service.IhcsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 @RestController
@@ -14,6 +21,9 @@ public class IhcsController {
 
     @Autowired
     private IhcsService ihcsService;
+
+    @Value("${web.printTablePath}")
+    private String printTablePath;
 
     @CrossOrigin
     @RequestMapping(value = "/all", method = RequestMethod.GET)
@@ -41,5 +51,33 @@ public class IhcsController {
     public Integer del(@RequestBody Ihcs ihcs) {
         System.out.println(ihcs);
         return this.ihcsService.updOne(ihcs);
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/print", method = RequestMethod.POST)
+    public String print(@RequestBody String printData) {
+        // 格式化数据
+        String result = printData.substring(11, printData.length() - 2);
+        // 转换js中的\r\n，Java中正则表达式\\\\代表\\,\\代表\
+        result = result.replaceAll("\\\\r\\\\n", "\r\n");
+        // 导出到指定文件
+        // 生成文件名
+        String fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance().getTime()) + ".txt";
+        File dist = new File(printTablePath + fileName);
+        if (!dist.getParentFile().exists()) {
+            // 创建
+            dist.getParentFile().mkdirs();
+        }
+        try {
+            FileWriter fw = new FileWriter(dist);
+            fw.write(result);
+//            fw.write("12\r\n34");
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return "成功打印标签。";
     }
 }
